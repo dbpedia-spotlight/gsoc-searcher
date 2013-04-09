b22a6e8105bedaac5360ab68b6bb6715dee9d622
Max Jakob <max.jakob@gmail.com>
2013-04-10 00:10:12 +0200

    save intermediate results into files
    so that processing does not have to start from the beginning each time

--- python/extract-gsoc-searcher-data.py
+++ python/extract-gsoc-searcher-data.py
@@ -1,12 +1,15 @@
+import os.path
 import csv
 import re
 import urllib
 import urllib2
+import socket
 import sys
 from boilerpipe.extract import Extractor
 
 
 csvInput = sys.argv[1]
+DATA_DIR = "data"
 
 ns = "http://spotlight.dbpedia.org/gsoc/vocab#"
 objectProperty = ns + "tagged"
@@ -30,19 +33,24 @@ def ntObject(s, p, o):
 def tsvFields(s, p, o):
     return s + "\t" + p.replace(ns, "") + "\t" + o
 
-
 serializer = tsvFields
+
+
+def getPath(fileName):
+    if not os.path.isdir(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    return os.path.join(DATA_DIR, fileName)
+
+def save(path, content):
+    with open(path, "a") as f:
+        f.write(content + "\n")
+
 
 def iterGsoc(csvFileName):
     """Generator for the parsed lines.
     """
-    skippedFirst = False
     with open(csvFileName) as csvFile:
-        for els in csv.reader(csvFile, delimiter=",", quotechar='"'):
-            if not skippedFirst:
-                skippedFirst = True
-                continue
-
+        for els in list(csv.reader(csvFile, delimiter=",", quotechar='"'))[1:]:
             key = els[0]
             name = els[1]
             linkId = els[2]
@@ -52,22 +60,22 @@ def iterGsoc(csvFileName):
             yield (key, name, linkId, tags, ideasUrl)
 
 
-def printProperties(ideasUrl, key, name, linkId):
+def saveProperties(path, ideasUrl, key, name, linkId):
     """General properties.
     """
-    print serializer(ideasUrl, keyProperty, key)
-    print serializer(ideasUrl, nameProperty, name)
-    print serializer(ideasUrl, linkIdProperty, linkId)
+    save(path, serializer(ideasUrl, keyProperty, key))
+    save(path, serializer(ideasUrl, nameProperty, name))
+    save(path, serializer(ideasUrl, linkIdProperty, linkId))
 
-def printTags(tags):
+def saveTags(path, tags):
     """Tags as strings.
     """
     for tag in tags:
         if tag.strip():
-            print serializer(ideasUrl, datatypeProperty, tag)
+            save(path, serializer(ideasUrl, datatypeProperty, tag))
 
-def _urisFromUrl(url, timeout=10, data=None):
-    sys.stderr.write("  request: "+url[:120]+"\n")
+def _urisFromUrl(url, timeout=30, data=""):
+    sys.stderr.write("  request: " + url[:120] + data[:30] + "...\n")
     """
     req = urllib2.urlopen(ideasHtml)
     encoding = getEncoding(req)
@@ -82,7 +90,7 @@ def _urisFromUrl(url, timeout=10, data=None):
     contents = urllib2.urlopen(request, timeout=timeout).read()
     return re.findall('"@URI": "(.*?)",', contents)
 
-def printDisambigTags(tags):
+def saveDisambigTags(path, tags):
     """Disambiuated tags.
     """
     uris = []
@@ -105,31 +113,45 @@ def printDisambigTags(tags):
         uris = _urisFromUrl(url)
 
     for uri in uris:
-        print serializer(ideasUrl, objectProperty, uri)
+        save(path, serializer(ideasUrl, objectProperty, uri))
 
-def printTextEntities(ideasUrl):
+def saveTextEntities(path, ideasUrl):
     """Disambiguated entities from the ideas page.
     """
-    url = urlPrefixSpotlight + "&url=" + ideasUrl
     try:
+        url = urlPrefixSpotlight + "&url=" + ideasUrl
         uris = _urisFromUrl(url)
     except urllib2.HTTPError:
-        sys.stderr.write("404!!!!!!!!!!!!!!!!!!\n")
+        sys.stderr.write("    produced HTTPError\n")
         extractor = Extractor(extractor='ArticleExtractor', url=ideasUrl)
         text = urllib.quote_plus(extractor.getText().replace("\n", "  ").encode("utf-8"))
         postParams = urllib.urlencode({"text": text}) 
-        uris = _urisFromUrl(urlPrefixSpotlight, timeout=30, data=postParams)
+        try:
+            uris = _urisFromUrl(urlPrefixSpotlight, timeout=120, data=postParams)
+        except socket.timeout:
+            uris = ["timed out"]
+
     for uri in uris:
-        print serializer(ideasUrl, textTaggedProperty, uri)
+        save(path, serializer(ideasUrl, textTaggedProperty, uri))
 
 
 if __name__ == "__main__":
     for key, name, linkId, tags, ideasUrl in iterGsoc(csvInput):
-        sys.stderr.write(ideasUrl + "\n")
-        printProperties(ideasUrl, key, name, linkId)
-        printTags(tags)
-        printDisambigTags(tags)
-        printTextEntities(ideasUrl)
+        sys.stderr.write(linkId + " " + ideasUrl)
 
+        fileName = getPath(linkId)
+        if os.path.exists(fileName):
+            sys.stderr.write("   exists\n")
+            continue
+        sys.stderr.write("\n")
 
+        tmpFileName = getPath(linkId + ".tmp")
+        open(tmpFileName, "w").write("")
+
+        saveProperties(tmpFileName, ideasUrl, key, name, linkId)
+        saveTags(tmpFileName, tags)
+        saveDisambigTags(tmpFileName, tags)
+        saveTextEntities(tmpFileName, ideasUrl)
+
+        os.rename(tmpFileName, fileName)
 
