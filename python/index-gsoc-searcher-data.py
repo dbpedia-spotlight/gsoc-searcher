e5f640e0c51c72efadee1476473e3d0fd15da5b4
Max Jakob <max.jakob@gmail.com>
2013-04-18 23:24:47 +0200

    proper grouping to index correctly
    - also STORE all fields in Lucene

--- python/index-gsoc-searcher-data.py
+++ python/index-gsoc-searcher-data.py
@@ -12,6 +12,36 @@ import re
 
 INDEX_ID = "gsoc2013"
 TYPE_ID = "d"
+MAPPING = """{
+  "d" : {
+    "properties" : {
+      "ideas" : {
+        "type" : "string",
+        "store" : "yes"
+      },
+      "linkId" : {
+        "type" : "string",
+        "store" : "yes"
+      },
+      "name" : {
+        "type" : "string",
+        "store" : "yes"
+      },
+      "tagged" : {
+        "type" : "string",
+        "store" : "yes"
+      },
+      "taggedString" : {
+        "type" : "string",
+        "store" : "yes"
+      },
+      "textTagged" : {
+        "type" : "string",
+        "store" : "yes"
+      }
+    }
+  }
+}"""
 
 
 class RequestWithMethod(urllib2.Request):
@@ -35,26 +65,45 @@ class RequestWithMethod(urllib2.Request):
 def analyzeKey(k):
     return k.replace("/", "_")
 
-def iterDataPoints(dataDir):
+def iterOrgData(dataDir):
+    d = {}  # group org data
     for fileName in [fn for fn in os.listdir(dataDir) if not fn.endswith(".text")]:
         fullPath = os.path.join(dataDir, fileName)
         with open(fullPath) as f:
             for line in f:
-                url, prop, val = line.strip().split("\t")
-                yield url, prop, val
+                key, prop, val = line.strip().split("\t")
+                d.setdefault(key, []).append((prop, val))
+    for k, vals in d.iteritems():
+        yield k, vals
 
-def indexDataPoint(elasticSearchUrl, dataPoint):
-    key, prop, val = dataPoint
+def wipeIndex(elasticSearchUrl):
+    print "Wiping index %s" % elasticSearchUrl
+    url = "%s/%s" % (elasticSearchUrl, INDEX_ID)
+    try:
+        print urllib2.urlopen(RequestWithMethod('DELETE', url)).read()
+    except urllib2.HTTPError, err:
+        if err.code != 404:
+            raise  # none existed
+    urllib2.urlopen(RequestWithMethod('PUT', url)).read()
+
+def setMapping(elasticSearchUrl):
+    url = "%s/%s/%s/_mapping" % (elasticSearchUrl, INDEX_ID, TYPE_ID)
+    print urllib2.urlopen(RequestWithMethod('PUT', url, data=MAPPING)).read()
+
+def indexOrgData(elasticSearchUrl, orgData):
+    key, vals = orgData
+    print "Indexing in %s: %s" % (elasticSearchUrl, key)
     url = "%s/%s/%s/%s" % (elasticSearchUrl, INDEX_ID, TYPE_ID, analyzeKey(key))
-    putJson = '{"%s":"%s"}' % (prop, val)
-    print "Indexing in %s: %s" % (elasticSearchUrl, dataPoint)
-    request = RequestWithMethod('PUT', url, data=putJson)
-    answerJson = urllib2.urlopen(request).read()
+    data = '{' + ",".join(['"%s":"%s"' % (prop, val) for prop, val in vals]) + '}'
+    answerJson = urllib2.urlopen(RequestWithMethod('PUT', url, data=data)).read()
     if not '"ok":true' in answerJson:
-        print url, putJson
-        print dataPoint, "failed indexation"
+        print url, data
+        print orgData, "failed indexation"
         sys.exit(1)
 
+def flushIndex(elasticSearchUrl):
+    url = "%s/_flush" % (elasticSearchUrl)
+    print urllib2.urlopen(RequestWithMethod('POST', url)).read()
 
 if __name__ == "__main__":
     dataDir = sys.argv[1]
@@ -62,6 +111,11 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         elasticSearchUrl = re.sub("/$", "", sys.argv[2])
 
-    for dataPoint in iterDataPoints(dataDir):
-        indexDataPoint(elasticSearchUrl, dataPoint)
+    wipeIndex(elasticSearchUrl)
+    setMapping(elasticSearchUrl)
+
+    for orgData in iterOrgData(dataDir):
+        indexOrgData(elasticSearchUrl, orgData)
+
+    flushIndex(elasticSearchUrl)
 
